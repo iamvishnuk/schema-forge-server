ac451328504367929d95ca1354d0ed42ce11172e
VISHNU K <86109601+iamvishnuk@users.noreply.github.com>
2025-03-21 16:30:13 +0530

    chore: add basic server with mongdb, husky, and commit lint

---
+++ commitlint.config.ts
@@ -0,0 +1,31 @@
+import type { UserConfig } from '@commitlint/types';
+
+const Configuration: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'type'
+      ]
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+    'subject-max-length': [2, 'always', 100]
+  }
+};
+
+export default Configuration;
